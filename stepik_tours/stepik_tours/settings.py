adef9818270e7f4610293e0ca012545eb038c7a6
laevsti <l.a.evsti@gmail.com>
2022-04-10 22:53:31 +0300

    second commit

--- stepik_tours/stepik_tours/settings.py
+++ stepik_tours/stepik_tours/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-cf#j=^_64(k!t6!w56m=s)ng9$5pies2-g)=epub2s9qm566(_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -105,6 +105,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
